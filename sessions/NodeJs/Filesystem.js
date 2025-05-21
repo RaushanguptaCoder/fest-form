600fb74ad44b01153cee3216cb8141e26b8f74ef
RaushanguptaCoder <roshangupta837@gmail.com>
2025-05-21 17:07:25 +0530

    filesystem(fs), import export, reactJS -->Vite

---
+++ sessions/NodeJs/Filesystem.js
@@ -0,0 +1,16 @@
+import fs from "fs" ;
+
+// write file
+// fs.writeFileSync("raushan.pdf","hello....\n");
+
+// // append file
+// fs.appendFileSync("raushan.pdf", "\n hiiiii....");
+
+// // read file 
+// let data = fs.readFileSync("raushan.pdf", "utf-8");
+// console.log(data);
+
+// delete file 
+fs.unlinkSync("raushan.pdf",)
+
+
---
+++ sessions/NodeJs/import.js
@@ -0,0 +1,5 @@
+import maths from "./export.js";
+console.log(maths.add(2,3));
+console.log(maths.sub(2,3));
+console.log(maths.mul(2,3));
+console.log(maths.div(2,3));
